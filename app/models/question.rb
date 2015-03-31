57f86dea1dbd15be0c35dd701094b10528275811
Kathryn Carr and Luke Chinworth <lukechinworth@gmail.com>
2015-03-31 16:52:14 -0700

    Sorting of votes to sort of work if best has been selected

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,10 @@ class Question < ActiveRecord::Base
   belongs_to :user
 
   def bestFirst
-    self.responses.sort_by {|obj| obj.best ? 0 : 1}    
+    best_first = self.responses.sort_by {|obj| obj.best ? 0 : 1}
+    responses_to_return = []
+    responses_to_return.push(best_first.pop)
+    binding.pry
+    responses_to_return + best_first.sort_by { |obj| obj.get_likes.size }.reverse
   end
 end
